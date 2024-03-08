bc75dbaf4f8b67bd191a5919058923af0866d026
Laraujo <88091434+lucara999@users.noreply.github.com>
2024-03-08 18:43:54 +0000

    upgrade print

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,7 @@
 --   By: laraujo <laraujo@student.42lyon.fr>        +#+  +:+       +#+        --
 --                                                +#+#+#+#+#+   +#+           --
 --   Created: 2024/03/07 18:26:08 by laraujo           #+#    #+#             --
---   Updated: 2024/03/08 17:21:43 by laraujo          ###   ########lyon.fr   --
+--   Updated: 2024/03/08 18:41:57 by laraujo          ###   ########lyon.fr   --
 --                                                                            --
 -- ************************************************************************** --
 
@@ -23,6 +23,7 @@ data InfiniteTape = InfiniteTape {
 
 data TuringMachine = TuringMachine {
   print_size :: Int,
+  len_input :: Int,
   states :: [String],
   alphabet :: [Char],
   init_state :: String,
@@ -61,6 +62,18 @@ printInfiniteTape tape size = do
   putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
   putStrLn $ take (size * 2) (right tape) ++ resetColor
 
+printStepTuring :: String -> InfiniteTape -> Int -> String -> IO ()
+printStepTuring prefix tape size detail = do
+  let resetColor = "\ESC[0m"
+  let bold = "\ESC[1m"
+  let rev = "\ESC[7m"
+  let grey_back = "\ESC[100m"
+
+  putStr $ prefix ++ grey_back ++ reverse (take (size + 2) (left tape))
+  putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
+  putStrLn $ take (size * 2) (right tape) ++ resetColor ++ "\t" ++ detail
+
+
 writeCursor :: InfiniteTape -> Char -> InfiniteTape
 writeCursor (InfiniteTape left' _ right') symbole = InfiniteTape left' symbole right'
 
@@ -88,21 +101,28 @@ findTransition cursor' state' transitions' = head [x | x <- transitions', state_
 
 runTuringMachine :: InfiniteTape -> TuringMachine -> String -> IO () --(InfiniteTape, TuringMachine)
 runTuringMachine tape turing_machine actual_state
-  | actual_state `elem` finals_state turing_machine = putStrLn actual_state
-  | otherwise = do 
+  | actual_state `elem` finals_state turing_machine = do
+    putStrLn "Final State reached :"
+    printInfiniteTape tape (len_input turing_machine)
+  | otherwise = do
     let transition = findTransition (cursor tape) actual_state (transitions turing_machine)
     let tape' = writeCursor tape (write_cur transition)
     let tape'' = mvAction tape' (action transition)
-    printInfiniteTape tape'' (print_size turing_machine)
+
+    let str = "(" ++ state_key transition ++ ", " ++ [read_cur transition] ++ ") \t\t‾‾|"
+    let str' = "(" ++ to_state transition ++ ", " ++ [write_cur transition] ++ ", " ++ action transition ++ ") \t__|"
+    printStepTuring "|‾‾\t" tape (print_size turing_machine) str
+    printStepTuring "|__\t" tape' (print_size turing_machine) str'
+
     runTuringMachine tape'' turing_machine (to_state transition)
 
 main :: IO ()
 main = do
-  putStrLn "###############################################"
-  putStrLn "#---------------------------------------------#"
-  putStrLn "#---------------- FT_TURING ------------------#"
-  putStrLn "#---------------------------------------------#"
-  putStrLn "#                                             #"
+  putStrLn "#############################################################"
+  putStrLn "#-----------------------------------------------------------#"
+  putStrLn "#----------------------- FT_TURING -------------------------#"
+  putStrLn "#-----------------------------------------------------------#"
+  putStrLn "#                                                           #"
 
   args <- getArgs
   if length args < 2 then do
@@ -110,16 +130,18 @@ main = do
     exitSuccess
   else
     putStrLn ""
+  let max_print_size = 10
   let name_file = head args
   let input_str = args !! 1
-  let len_input = length input_str
+  let len_print = min (length input_str) max_print_size
   putStrLn $ "FileName : " ++ name_file
   putStrLn $ "Input_str : " ++ input_str ++ "\n"
 
   let blank = '.'
 
   let turing_machine = TuringMachine {
-    print_size = len_input,
+    print_size = len_print,
+    len_input = length input_str,
     states = ["impaire", "paire", "HALT", "STOP"],
     alphabet = ['0', '.'],
     init_state = "impaire",
@@ -131,8 +153,10 @@ main = do
       Transition "paire" '.' "HALT" 'n' "RIGHT"]
   }
 
+  putStrLn "Initial Turing Machine :"
   let tape = initInfiniteTape input_str blank
-  printInfiniteTape tape len_input
+  printInfiniteTape tape (length input_str)
+  putStrLn "Running Turing Machine :"
 
   runTuringMachine tape turing_machine (init_state turing_machine)
   -- printInfiniteTape tape' len_input
