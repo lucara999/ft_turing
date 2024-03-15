0fed977dcd4cd5e0ff19b248f02a84ad4e129f7c
M-ROZNIECKI <mrozniec@student.42lyon.fr>
2024-03-15 14:06:32 +0100

    Fix du parser de JSON et ajout des modules necessaires dans le ft-turing.cabal

---
+++ src/JSONparsing.hs
@@ -0,0 +1,140 @@
+-- ************************************************************************** --
+--                                                          LE - /            --
+--                                                              /             --
+--   JSONparsing.hs                                   .::    .:/ .      .::   --
+--                                                 +:+:+   +:    +:  +:+:+    --
+--   By: mrozniec <mrozniec@student.42lyon.fr>      +:+   +:    +:    +:+     --
+--                                                 #+#   #+    #+    #+#      --
+--   Created: 2024/03/08 15:03:08 by mrozniec     #+#   ##    ##    #+#       --
+--   Updated: 2024/03/13 17:50:28 by mrozniec    ###    #+. /#+    ###.fr     --
+--                                                         /                  --
+--                                                        /                   --
+-- ************************************************************************** --
+{-# LANGUAGE DeriveGeneric #-}
+
+module JSONparsing (parseTuring, TuringMachine, Transition) where
+
+import Data.Aeson(FromJSON, decode)
+import Data.Map
+import Data.ByteString.Lazy (readFile)
+import GHC.Generics(Generic)
+
+data TuringMachine = TuringMachine {
+    name :: String,
+    alphabet :: [String],
+    blank :: String,
+    states :: [String],
+    initial :: String,
+    finals :: [String],
+    transitions :: Map String [Transition]
+} deriving (Show, Generic)
+instance FromJSON TuringMachine
+
+data Transition = Transition {
+    read :: String,
+    to_state :: String,
+    write :: String,
+    action :: String
+} deriving (Show, Generic)
+instance FromJSON Transition
+
+loadJSON :: String -> IO ( Maybe TuringMachine )
+loadJSON fileName = do
+    jsonString <- Data.ByteString.Lazy.readFile fileName
+    return (decode jsonString)
+
+validate :: String -> TuringMachine -> Maybe String
+validate input tm = do
+
+    --  verification if name is empty
+    if (length (name tm)) == 0 then
+        Just "empty name inside JSON"
+
+    --  verification if the list of the character of the alphabet is empty
+    else if (length (alphabet tm)) == 0 then
+        Just "empty alphabet inside JSON"
+
+    --  verification if the character of the alphabet are only 1 char long
+    else if (any (\str -> length str /= 1) (alphabet tm)) then
+        Just "every character of the alphabet inside the json must be 1 char"
+
+    --  verification if blank is just 1 char long and not empty
+    else if (length (blank tm)) /= 1 then
+        Just "blank must be 1 char"
+
+    --  verification if blank is part of the alphabet
+    else if (blank tm) `notElem` (alphabet tm) then
+        Just "blank must be a part of the alphabet"
+
+    --  verification if the list of states is not empty
+    else if (length (states tm)) == 0 then
+        Just "empty states list inside JSON"
+
+    --  verification if there is an initial state
+    else if (length (initial tm)) == 0 then
+        Just "empty initial state inside JSON"
+
+    --  verification if the initial state is part of the list of states
+    else if (initial tm) `notElem` (states tm) then
+        Just "initial state not part of the states inside JSON"
+
+    --  verification if there is finals states
+    else if (length (finals tm)) == 0 then
+        Just "empty finals states list inside JSON"
+
+    --  verification if the finals states are part of the list of states
+    else if (any (\str -> str `notElem` (states tm)) (finals tm)) then
+        Just "there is finals states that are not part of the list of states inside the JSON"
+
+    --  verification if there is transitions
+    else if (length (transitions tm)) == 0 then
+        Just "empty Map of transitions inside JSON"
+
+    --  verification if the transitions states are part of the list of states
+    else if (any (\str -> str `notElem` (states tm)) (keys (transitions tm))) then
+        Just "a list of transition is linked to an invalid state inside the field transitions of the JSON"
+
+    --  verification if the read inside the transitions are part of the alphabet
+    else if (any (\l -> (JSONparsing.read l) `notElem` (alphabet tm)) (concat (elems (transitions tm)))) then
+        Just "the charactere read inside a transition is not inside the alphabet given inside the JSON"
+
+    --  verification if the write inside the transitions are part of the alphabet
+    else if (any (\l -> (JSONparsing.write l) `notElem` (alphabet tm)) (concat (elems (transitions tm)))) then
+        Just "the charactere write inside a transition is not inside the alphabet given inside the JSON"
+
+    --  verification if the to_state state inside the transitions are part of the list of states
+    else if (any (\l -> (to_state l) `notElem` (states tm)) (concat (elems (transitions tm)))) then
+        Just "the state of to_state inside a transition is not inside the states listed inside the JSON"
+
+    --  verification if the input is empty
+    else if (length input) == 0 then
+        Just "input empty"
+
+    --  verification if every char of the input are part of the alphabet
+    else if (any (\c -> [c] `notElem` (alphabet tm)) input) then
+        Just "input contain a character not part of the alphabet defined inside the JSON"
+
+    --  verification if there is no blank character inside the input
+    else if (head (blank tm)) `elem` input then
+        Just "input contain a blank character"
+
+    --  OK
+    else
+        Nothing
+
+
+parseTuring :: String -> String -> IO (Maybe TuringMachine)
+parseTuring input fileName = do
+    turingMachine <- loadJSON fileName
+    case turingMachine of
+        Nothing -> do
+            putStrLn "Error parsing JSON file"
+            return Nothing
+        Just tm -> do
+            case validate input tm of
+                Just err -> do
+                    putStrLn ("Error: " ++ err)
+                    return Nothing
+                Nothing -> do
+                    putStrLn "Parsing JSON done"
+                    return turingMachine
--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,7 @@ module Main where
 import System.Environment (getArgs)
 import System.Exit
 -- import qualified MyLib (someFunc)
+import JSONparsing(parseTuring, TuringMachine, Transition)
 
 data InfiniteTape = InfiniteTape {
   left :: [Char],   -- LazyList
@@ -21,100 +22,100 @@ data InfiniteTape = InfiniteTape {
   right :: [Char]   -- LazyList
 }
 
-data TuringMachine = TuringMachine {
-  print_size :: Int,
-  len_input :: Int,
-  states :: [String],
-  alphabet :: [Char],
-  init_state :: String,
-  finals_state :: [String],
-  transitions :: Transitions
-}
-
-type Action = String
-
-data Transition = Transition {
-  state_key :: String,
-  read_cur :: Char,
-  to_state :: String,
-  write_cur :: Char,
-  action :: Action
-}
-
-type Transitions = [Transition]
-
-initInfiniteTape :: [Char] -> Char -> InfiniteTape
-initInfiniteTape input_str blank = InfiniteTape {
-  left = repeat blank,
-  cursor = head input_str,
-  right = tail input_str ++ repeat blank
-}
-
-printInfiniteTape :: InfiniteTape -> Int -> IO ()
-printInfiniteTape tape size = do
-  let resetColor = "\ESC[0m"
-  let bold = "\ESC[1m"
-  let rev = "\ESC[7m"
-  let grey_back = "\ESC[100m"
-  let tab = "\t"
-
-  putStr $ tab ++ grey_back ++ reverse (take (size + 2) (left tape))
-  putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
-  putStrLn $ take (size * 2) (right tape) ++ resetColor
-
-printStepTuring :: String -> InfiniteTape -> Int -> String -> IO ()
-printStepTuring prefix tape size detail = do
-  let resetColor = "\ESC[0m"
-  let bold = "\ESC[1m"
-  let rev = "\ESC[7m"
-  let grey_back = "\ESC[100m"
-
-  putStr $ prefix ++ grey_back ++ reverse (take (size + 2) (left tape))
-  putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
-  putStrLn $ take (size * 2) (right tape) ++ resetColor ++ "\t" ++ detail
-
-
-writeCursor :: InfiniteTape -> Char -> InfiniteTape
-writeCursor (InfiniteTape left' _ right') symbole = InfiniteTape left' symbole right'
-
-mvCursorLeft :: InfiniteTape -> InfiniteTape
-mvCursorLeft (InfiniteTape [] cursor' right') = InfiniteTape [] cursor' right'
-mvCursorLeft (InfiniteTape (l:left') cursor' right') = InfiniteTape left' l (cursor':right')
-
-mvCursorRight :: InfiniteTape -> InfiniteTape
-mvCursorRight (InfiniteTape left' cursor' []) = InfiniteTape left' cursor' []
-mvCursorRight (InfiniteTape left' cursor' (r:right')) = InfiniteTape (cursor':left') r right'
-
-mvAction :: InfiniteTape -> Action -> InfiniteTape
-mvAction tape actionStr
-  | actionStr == "LEFT" = mvCursorLeft tape
-  | actionStr == "RIGHT" = mvCursorRight tape
-  | otherwise = tape
-
-mvRightSearch :: InfiniteTape -> Char -> InfiniteTape
-mvRightSearch tape search
-  | cursor tape == search = tape
-  | otherwise = mvRightSearch (mvCursorRight tape) search
-
-findTransition :: Char -> String -> Transitions -> Transition
-findTransition cursor' state' transitions' = head [x | x <- transitions', state_key x == state', read_cur x == cursor']
-
-runTuringMachine :: InfiniteTape -> TuringMachine -> String -> IO () --(InfiniteTape, TuringMachine)
-runTuringMachine tape turing_machine actual_state
-  | actual_state `elem` finals_state turing_machine = do
-    putStrLn "Final State reached :"
-    printInfiniteTape tape (len_input turing_machine)
-  | otherwise = do
-    let transition = findTransition (cursor tape) actual_state (transitions turing_machine)
-    let tape' = writeCursor tape (write_cur transition)
-    let tape'' = mvAction tape' (action transition)
-
-    let str = "(" ++ state_key transition ++ ", " ++ [read_cur transition] ++ ") \t\t‾‾|"
-    let str' = "(" ++ to_state transition ++ ", " ++ [write_cur transition] ++ ", " ++ action transition ++ ") \t__|"
-    printStepTuring "|‾‾\t" tape (print_size turing_machine) str
-    printStepTuring "|__\t" tape' (print_size turing_machine) str'
-
-    runTuringMachine tape'' turing_machine (to_state transition)
+-- data TuringMachine = TuringMachine {
+--   print_size :: Int,
+--   len_input :: Int,
+--   states :: [String],
+--   alphabet :: [Char],
+--   init_state :: String,
+--   finals_state :: [String],
+--   transitions :: Transitions
+-- }
+-- 
+-- type Action = String
+-- 
+-- data Transition = Transition {
+--   state_key :: String,
+--   read_cur :: Char,
+--   to_state :: String,
+--   write_cur :: Char,
+--   action :: Action
+-- }
+-- 
+-- type Transitions = [Transition]
+--
+--initInfiniteTape :: [Char] -> Char -> InfiniteTape
+--initInfiniteTape input_str blank = InfiniteTape {
+--  left = repeat blank,
+--  cursor = head input_str,
+--  right = tail input_str ++ repeat blank
+--}
+--
+--printInfiniteTape :: InfiniteTape -> Int -> IO ()
+--printInfiniteTape tape size = do
+--  let resetColor = "\ESC[0m"
+--  let bold = "\ESC[1m"
+--  let rev = "\ESC[7m"
+--  let grey_back = "\ESC[100m"
+--  let tab = "\t"
+--
+--  putStr $ tab ++ grey_back ++ reverse (take (size + 2) (left tape))
+--  putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
+--  putStrLn $ take (size * 2) (right tape) ++ resetColor
+--
+--printStepTuring :: String -> InfiniteTape -> Int -> String -> IO ()
+--printStepTuring prefix tape size detail = do
+--  let resetColor = "\ESC[0m"
+--  let bold = "\ESC[1m"
+--  let rev = "\ESC[7m"
+--  let grey_back = "\ESC[100m"
+--
+--  putStr $ prefix ++ grey_back ++ reverse (take (size + 2) (left tape))
+--  putStr $ bold ++ rev ++ [cursor tape] ++ resetColor ++ grey_back
+--  putStrLn $ take (size * 2) (right tape) ++ resetColor ++ "\t" ++ detail
+--
+--
+--writeCursor :: InfiniteTape -> Char -> InfiniteTape
+--writeCursor (InfiniteTape left' _ right') symbole = InfiniteTape left' symbole right'
+--
+--mvCursorLeft :: InfiniteTape -> InfiniteTape
+--mvCursorLeft (InfiniteTape [] cursor' right') = InfiniteTape [] cursor' right'
+--mvCursorLeft (InfiniteTape (l:left') cursor' right') = InfiniteTape left' l (cursor':right')
+--
+--mvCursorRight :: InfiniteTape -> InfiniteTape
+--mvCursorRight (InfiniteTape left' cursor' []) = InfiniteTape left' cursor' []
+--mvCursorRight (InfiniteTape left' cursor' (r:right')) = InfiniteTape (cursor':left') r right'
+--
+--mvAction :: InfiniteTape -> Action -> InfiniteTape
+--mvAction tape actionStr
+--  | actionStr == "LEFT" = mvCursorLeft tape
+--  | actionStr == "RIGHT" = mvCursorRight tape
+--  | otherwise = tape
+--
+--mvRightSearch :: InfiniteTape -> Char -> InfiniteTape
+--mvRightSearch tape search
+--  | cursor tape == search = tape
+--  | otherwise = mvRightSearch (mvCursorRight tape) search
+--
+--findTransition :: Char -> String -> Transitions -> Transition
+--findTransition cursor' state' transitions' = head [x | x <- transitions', state_key x == state', read_cur x == cursor']
+--
+--runTuringMachine :: InfiniteTape -> TuringMachine -> String -> IO () --(InfiniteTape, TuringMachine)
+--runTuringMachine tape turing_machine actual_state
+--  | actual_state `elem` finals_state turing_machine = do
+--    putStrLn "Final State reached :"
+--    printInfiniteTape tape (len_input turing_machine)
+--  | otherwise = do
+--    let transition = findTransition (cursor tape) actual_state (transitions turing_machine)
+--    let tape' = writeCursor tape (write_cur transition)
+--    let tape'' = mvAction tape' (action transition)
+--
+--    let str = "(" ++ state_key transition ++ ", " ++ [read_cur transition] ++ ") \t\t‾‾|"
+--    let str' = "(" ++ to_state transition ++ ", " ++ [write_cur transition] ++ ", " ++ action transition ++ ") \t__|"
+--    printStepTuring "|‾‾\t" tape (print_size turing_machine) str
+--    printStepTuring "|__\t" tape' (print_size turing_machine) str'
+--
+--    runTuringMachine tape'' turing_machine (to_state transition)
 
 main :: IO ()
 main = do
@@ -139,26 +140,33 @@ main = do
 
   let blank = '.'
 
-  let turing_machine = TuringMachine {
-    print_size = len_print,
-    len_input = length input_str,
-    states = ["impaire", "paire", "HALT", "STOP"],
-    alphabet = ['0', '.'],
-    init_state = "impaire",
-    finals_state = ["HALT", "STOP"],
-    transitions = [
-      Transition "impaire" '0' "paire" '0' "RIGHT",
-      Transition "impaire" '.' "HALT" 'y' "RIGHT",
-      Transition "paire" '0' "impaire" '0' "RIGHT",
-      Transition "paire" '.' "HALT" 'n' "RIGHT"]
-  }
+  -- let turing_machine = TuringMachine {
+  --   print_size = len_print,
+  --   len_input = length input_str,
+  --   states = ["impaire", "paire", "HALT", "STOP"],
+  --   alphabet = ['0', '.'],
+  --   init_state = "impaire",
+  --   finals_state = ["HALT", "STOP"],
+  --   transitions = [
+  --     Transition "impaire" '0' "paire" '0' "RIGHT",
+  --     Transition "impaire" '.' "HALT" 'y' "RIGHT",
+  --     Transition "paire" '0' "impaire" '0' "RIGHT",
+  --     Transition "paire" '.' "HALT" 'n' "RIGHT"]
+  -- }
+  turing_machine <- parseTuring input_str name_file
+  case (turing_machine) of
+    Nothing -> do
+      putStrLn "exit"
+    Just tm -> do
+      putStrLn (show tm)
 
-  putStrLn "Initial Turing Machine :"
-  let tape = initInfiniteTape input_str blank
-  printInfiniteTape tape (length input_str)
-  putStrLn "Running Turing Machine :"
 
-  runTuringMachine tape turing_machine (init_state turing_machine)
+  --putStrLn "Initial Turing Machine :"
+  --let tape = initInfiniteTape input_str blank
+  --printInfiniteTape tape (length input_str)
+  --putStrLn "Running Turing Machine :"
+--
+  --runTuringMachine tape turing_machine (init_state turing_machine)
   -- printInfiniteTape tape' len_input
 
   -- let tape1 = mvRightSearch tape '='
